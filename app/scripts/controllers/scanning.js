5cf0d72ebd44459db408079111187d0f61c43b59
HU Pili <hpl1989@gmail.com>
2015-09-25 23:08:04 +0800

    playable version

--- app/scripts/controllers/scanning.js
+++ app/scripts/controllers/scanning.js
@@ -14,4 +14,43 @@ angular.module('neuqueninitiumdatacomApp')
       'AngularJS',
       'Karma'
     ];
+    $scope.test = function(){
+      alert('hel');
+    }
+
+    $('#mixer').keypress(function(){
+      if ( event.which == 'a'.charCodeAt(0) ) {
+        $scope.showStation1 = !$scope.showStation1;
+        console.log($scope.showStation1);
+        $scope.$apply();
+        event.preventDefault();
+        return ;
+      }
+      if ( event.which == 's'.charCodeAt(0) ) {
+        $scope.showStation2 = !$scope.showStation2;
+        event.preventDefault();
+        $scope.$apply();
+        return ;
+      }
+      if ( event.which == 'd'.charCodeAt(0) ) {
+        $scope.showStation3 = !$scope.showStation3;
+        event.preventDefault();
+        $scope.$apply();
+        return ;
+      }
+    })
   });
+
+//angular.directive('myEnter', function () {
+//  return function (scope, element, attrs) {
+//    element.bind("keydown keypress", function (event) {
+//      if(event.which === 13) {
+//        scope.$apply(function (){
+//          scope.$eval(attrs.myEnter);
+//        });
+//
+//        event.preventDefault();
+//      }
+//    });
+//  };
+//});
